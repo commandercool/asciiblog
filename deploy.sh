56aac717ba3df90d1d7999177a05093992beb440
aerokhin <Aleksandr.Erokhin@t-systems.com>
2021-06-07 12:15:16 +0300

    Fix deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 asciidoctor -R src -D target '**/*.adoc' && \
-rsync -av src/images blog:/var/www/html && \
-rsync -av target/ blog:/var/www/html
+rsync -av -e "ssh -l ubuntu" src/images blog:/var/www/html && \
+rsync -av -e "ssh -l ubuntu" target/ blog:/var/www/html
+
+# rsync -av -e "ssh -l ssh-user" rsync-user@host::module /dest
